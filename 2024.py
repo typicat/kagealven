cd51b36a2847aa5c62826e01b7c1aba9c8d39764
Karl Jonsson <kjonsson@gmail.com>
2025-05-08 16:54:36 +0200

    Latest

--- 2024.py
+++ 2024.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 url = "https://kagealven.com/2025/02/2024/"
-data = pd.read_html(url)[0]
+df = pd.read_html(url)[0]
 
 df = df.fillna('')
 df.to_csv('output_2024.csv', index=False, header=False)
--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import pandas as pd
 def main():
     df = pd.read_html(url)[0]
     df = df.fillna('')
+
     species_column = 2
     location_column = 10
     sum_art = df[species_column].value_counts()
     sum_loc = df[location_column].value_counts()
+
     print("Writing csv..")
     df.to_csv('output.csv', index=False, header=False)
+
     print(sum_art)
     print("-" * 35)
     print(sum_loc)
     print("Latest report:")
+
     latest_report = pd.DataFrame([df.iloc[0]])  # Convert the top row to a dataframe
     print(latest_report.to_string())
 
