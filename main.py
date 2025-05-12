4b0348eb34eb43d9e059a51443e685e9e9881c00
Karl Jonsson <kjonsson@gmail.com>
2025-05-12 19:47:32 +0200

    new batch

--- main.py
+++ main.py
@@ -2,22 +2,19 @@ import pandas as pd
 
 
 def main():
-    print("kagealven v0.1.0")
-    print("Fetching reports...")
+    print("kagealven v0.1.0 - fetching reports...")
     df = pd.read_html(url)[0]
     df = df.fillna("")
-
     species_column = 2
     location_column = 10
     sum_art = df[species_column].value_counts()
     sum_loc = df[location_column].value_counts()
 
     df.to_csv("output.csv", index=False, header=False)
-
     print(sum_art)
     print("-" * 35)
     print(sum_loc)
-    print("Latest report:")
+    print("Latest entry:")
 
     latest_report = pd.DataFrame([df.iloc[0]])
     print(latest_report.to_string())
