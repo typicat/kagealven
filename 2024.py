f136a9cc2094323e8709175d8feabd4d8f21b9c8
Karl Jonsson <kjonsson@gmail.com>
2025-08-19 22:36:57 +0200

    hello

--- 2024.py
+++ 2024.py
@@ -3,6 +3,5 @@ import pandas as pd
 
 url = "https://kagealven.com/2025/02/2024/"
 df = pd.read_html(url)[0]
-
-df = df.fillna('')
-df.to_csv('output_2024.csv', index=False, header=False)
+df = df.fillna("")
+df.to_csv("output_2024.csv", index=False, header=False)
