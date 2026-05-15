b258588bd03611f7781112726785f972c0b85e5d
Karl Jonsson <kjonsson@gmail.com>
2026-05-15 22:22:03 +0200

    remove 2024, we do not use it anymore fix User-Agent in main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from urllib.request import Request, urlopen
 SPECIES_COLUMN = 2
 LOCATION_COLUMN = 10
 FIREFOX_USER_AGENT = (
-    "Mozilla/5.0 (X11; Linux x86_64; rv:138.0) "
-    "Gecko/20100101 Firefox/138.0"
+    "Mozilla/5.0 (X11; Linux x86_64; rv:151.0) "
+    "Gecko/20100101 Firefox/151.0"
 )
 
 
